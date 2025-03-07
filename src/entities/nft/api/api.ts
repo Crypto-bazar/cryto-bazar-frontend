65fe5e4f715888e4084bafdd24584348a7d4a14f
Ororura <hitmanlol890@gmail.com>
2025-03-07 15:51:27 +0300

    feat(api) update API URL configuration and refactor NFT API calls

--- src/entities/nft/api/api.ts
+++ src/entities/nft/api/api.ts
@@ -1,10 +1,9 @@
+import { axiosInstance } from '@/shared/api';
 import { NFT } from '../types';
-import axios from 'axios';
 
 const getNFTs = async () => {
   try {
-    // const response = await axiosInstance.get<NFT[]>('/api/v1/nfts/');
-    const response = await axios.get<NFT[]>('http://localhost:8080/api/v1/nfts/');
+    const response = await axiosInstance.get<NFT[]>('/api/v1/nfts/');
     return response.data;
   } catch (e) {
     console.error(e);
--- src/entities/nft/ui/component.tsx
+++ src/entities/nft/ui/component.tsx
@@ -1,14 +1,23 @@
 import { FC } from 'react';
 import { NFT } from '../types';
+import Image from 'next/image';
 
 type Props = {
   nft: NFT;
 };
 
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
 const NFTCard: FC<Props> = ({ nft }) => {
   return (
     <div className='rounded-lg border p-4 shadow-lg'>
-      {/* <Image src={nft.image} alt={nft.name} className='mb-4 h-48 w-full rounded object-cover' /> */}
+      <Image
+        width={200}
+        height={200}
+        src={`${apiUrl}uploads/${nft.image_path}`}
+        alt={nft.Name}
+        className='mb-4 h-48 w-full rounded object-cover'
+      />
       <h2 className='mb-2 text-xl font-bold'>{nft.Name}</h2>
       <p className='mb-2 text-gray-700'>{nft.Description}</p>
       <p className='font-semibold text-gray-900'>Цена: {nft.Price} ETH</p>
