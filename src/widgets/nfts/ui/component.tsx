d1fcb3e5a64e28fbba06f6435ad83ae89d0e33c2
Ororura <hitmanlol890@gmail.com>
2025-03-07 18:14:20 +0300

    feat(create-nft) implement CreateNFT form with validation and input fields

--- src/widgets/nfts/ui/component.tsx
+++ src/widgets/nfts/ui/component.tsx
@@ -1,14 +1,18 @@
 import { FC } from 'react';
 import { NFTCard } from '@/entities/nft/ui';
 import { getNFTs } from '@/entities/nft/api';
+import { CreateNFT } from 'features/create-nft/ui';
 
 const NFTs: FC = async () => {
   const nfts = await getNFTs();
   return (
-    <div className='grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-3'>
-      {nfts.map((nft, index) => (
-        <NFTCard key={index} nft={nft} />
-      ))}
+    <div>
+      <CreateNFT />
+      <div className='grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-3'>
+        {nfts.map((nft, index) => (
+          <NFTCard key={index} nft={nft} />
+        ))}
+      </div>
     </div>
   );
 };
