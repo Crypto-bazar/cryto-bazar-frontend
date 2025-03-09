e370baa40840ee539a488b9626693c0bc27bb969
Ororura <hitmanlol890@gmail.com>
2025-03-09 16:52:37 +0300

    feat(dialog) add Dialog component with overlay, content, and related subcomponents

--- src/features/create-nft/ui/component.tsx
+++ src/features/create-nft/ui/component.tsx
@@ -1,6 +1,6 @@
 'use client';
-import { FC } from 'react';
-import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from 'shared/ui/form/ui';
+import { FC, useState } from 'react';
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from 'shared/ui/form/ui';
 import { FormProvider, useForm } from 'react-hook-form';
 import { Button } from 'shared/ui/button/ui';
 import { nftSchema } from 'features/create-nft/model';
@@ -8,8 +8,10 @@ import { Input } from 'shared/ui/input/ui';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { createNFTApi } from '../api/api';
+import { Dialog, DialogTrigger, DialogContent, DialogHeader } from '@/shared/ui/dialog/ui';
 
 const CreateNFT: FC = () => {
+  const [open, setOpen] = useState(false);
   const form = useForm<z.infer<typeof nftSchema>>({
     resolver: zodResolver(nftSchema),
     defaultValues: {
@@ -23,95 +25,94 @@ const CreateNFT: FC = () => {
   const onSubmit = async (data: z.infer<typeof nftSchema>) => {
     const formData = { ...data, owner_address: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266' };
     await createNFTApi(formData);
+    setOpen(false);
   };
 
   return (
-    <FormProvider {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className='space-y-8 rounded-[8px] bg-[#3b5a6b] px-5 py-5 text-white'
-      >
-        <FormField
-          control={form.control}
-          name='name'
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Название</FormLabel>
-              <FormControl>
-                <Input placeholder='Выберите название' {...field} />
-              </FormControl>
-              <FormDescription>This is your public display name.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name='description'
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Описание</FormLabel>
-              <FormControl>
-                <Input placeholder='Выберите описание' {...field} className='placeholder-slate-200' />
-              </FormControl>
-              <FormDescription>This is your public display name.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name='price'
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Цена</FormLabel>
-              <FormControl>
-                <Input placeholder='Выберите цену' {...field} />
-              </FormControl>
-              <FormDescription>This is your public display name.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name='image'
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Картинка</FormLabel>
-              <FormControl>
-                <Input
-                  id='image'
-                  type='file'
-                  placeholder='Выберите картинку'
-                  onChange={(e) => field.onChange(e.target.files?.[0])}
-                  onBlur={field.onBlur}
-                  name={field.name}
-                  ref={field.ref}
-                />
-              </FormControl>
-              <FormDescription>This is your public display name.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name='token_id'
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Токен</FormLabel>
-              <FormControl>
-                <Input placeholder='Введите адрес токена' {...field} />
-              </FormControl>
-              <FormDescription>This is your public display name.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type='submit'>Создать NFT</Button>
-      </form>
-    </FormProvider>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button variant='default'>Создать NFT</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>Создание NFT</DialogHeader>
+        <FormProvider {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
+            <FormField
+              control={form.control}
+              name='name'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Название</FormLabel>
+                  <FormControl>
+                    <Input placeholder='Введите название' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='description'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Описание</FormLabel>
+                  <FormControl>
+                    <Input placeholder='Введите описание' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='price'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Цена</FormLabel>
+                  <FormControl>
+                    <Input placeholder='Введите цену' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='image'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Картинка</FormLabel>
+                  <FormControl>
+                    <Input
+                      type='file'
+                      onChange={(e) => field.onChange(e.target.files?.[0])}
+                      onBlur={field.onBlur}
+                      name={field.name}
+                      ref={field.ref}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='token_id'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Токен</FormLabel>
+                  <FormControl>
+                    <Input placeholder='Введите адрес токена' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type='submit'>Создать NFT</Button>
+          </form>
+        </FormProvider>
+      </DialogContent>
+    </Dialog>
   );
 };
 
