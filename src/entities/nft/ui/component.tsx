ce523fc60f8f60fc2d7c73fd37477d676e4a61cb
Ororura <hitmanlol890@gmail.com>
2025-03-13 20:18:06 +0300

    feat(nft) implement Card component for NFT display and enhance NFTCard layout

--- src/entities/nft/ui/component.tsx
+++ src/entities/nft/ui/component.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import { NFT } from '../models/types';
 import Image from 'next/image';
 import { Button } from '@/shared/ui/button/ui';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/shared/ui/card/ui';
 
 type Props = {
   nft: NFT;
@@ -12,21 +13,31 @@ const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 const NFTCard: FC<Props> = ({ nft, onCreate }) => {
   return (
-    <div className='rounded-lg border p-4 shadow-lg'>
-      <Image
-        width={200}
-        height={200}
-        src={`${apiUrl}uploads/${nft.image_path}`}
-        alt={nft.name}
-        className='mb-4 h-48 w-full rounded object-cover'
-      />
-      <h2 className='mb-2 text-xl font-bold'>{nft.name}</h2>
-      <p className='mb-2 text-gray-700'>{nft.description}</p>
-      <p className='font-semibold text-gray-900'>Цена: {nft.price} ETH</p>
-      <Button onClick={() => onCreate(nft.id, nft.name, nft.symbol, `${apiUrl}uploads/${nft.image_path}`)}>
-        Токенезировать
-      </Button>
-    </div>
+    <Card className='shadow-lg'>
+      <CardHeader>
+        <Image
+          width={200}
+          height={200}
+          src={`${apiUrl}uploads/${nft.image_path}`}
+          alt={nft.name}
+          className='mb-4 h-48 w-full rounded object-cover'
+        />
+        <CardTitle>{nft.name}</CardTitle>
+        <CardDescription>{nft.token_id && 'Токенезирован'}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p className='mb-2 text-gray-700'>{nft.description}</p>
+        <p className='font-semibold text-gray-900'>Цена: {parseFloat(nft.price)} ETH</p>
+        {nft.token_id === '' && (
+          <Button
+            className='mt-2'
+            onClick={() => onCreate(nft.id, nft.name, nft.symbol, `${apiUrl}uploads/${nft.image_path}`)}
+          >
+            Токенезировать
+          </Button>
+        )}
+      </CardContent>
+    </Card>
   );
 };
 
