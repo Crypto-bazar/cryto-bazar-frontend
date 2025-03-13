88083a10f9b694abe5bdcae91e015a080e90e617
Ororura <hitmanlol890@gmail.com>
2025-03-13 19:37:13 +0300

    feat(nft) refactor useCreateToken hook to handle token ID state and update contract address on success

--- src/features/create-nft/api/web3.tsx
+++ src/features/create-nft/api/web3.tsx
@@ -2,27 +2,35 @@ import { abi } from '@/shared/api';
 import { useWaitForTransactionReceipt, useWriteContract } from 'wagmi';
 import { SetNFTContractAddress } from '../model';
 import { setNFTContractAddress } from './api';
+import { useEffect, useState } from 'react';
 
 const useCreateToken = () => {
   const { writeContractAsync, data: hash } = useWriteContract();
   const { data: receipt, isLoading, isSuccess } = useWaitForTransactionReceipt({ hash });
+  const [tokenId, setTokenId] = useState<number | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      if (isSuccess && receipt && tokenId !== null) {
+        const data: SetNFTContractAddress = {
+          id: String(tokenId),
+          contract_address: receipt?.logs[0].address,
+        };
+        const response = await setNFTContractAddress(data);
+
+        console.log(response?.data);
+      }
+    })();
+  }, [isSuccess, receipt, tokenId]);
 
   const createToken = async (id: number, name: string, symb: string, uri: string) => {
+    setTokenId(id);
     const txHash = await writeContractAsync({
       abi: abi,
       address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as `0x${string}`,
       functionName: 'createNFT',
       args: [name, symb, uri],
     });
-
-    const data: SetNFTContractAddress = {
-      id: String(id),
-      contract_address: receipt?.logs[0].address,
-    };
-
-    const response = await setNFTContractAddress(data);
-
-    console.log(response?.data);
 
     return txHash;
   };
