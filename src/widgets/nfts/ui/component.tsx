799db124bf2530cb90a318af6afaddfc6169073d
Ororura <hitmanlol890@gmail.com>
2025-03-14 21:45:34 +0300

    fix(nft) adjust padding in NFT grid layout for improved spacing

--- src/widgets/nfts/ui/component.tsx
+++ src/widgets/nfts/ui/component.tsx
@@ -24,7 +24,7 @@ const NFTs: FC = () => {
   return (
     <div>
       <CreateNFT />
-      <div className='grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-3'>
+      <div className='grid grid-cols-1 gap-4 py-4 md:grid-cols-2 lg:grid-cols-3'>
         {nfts && nfts.map((nft, index) => <NFTCard key={index} onCreate={handleCreateToken} nft={nft} />)}
       </div>
     </div>
