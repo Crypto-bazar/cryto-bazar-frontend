e49b04a11a6077a11a4ea75e089ea15690e861b6
Ororura <hitmanlol890@gmail.com>
2025-03-18 11:47:31 +0300

    feat(event): impl listen hook

---
+++ src/features/sell-nft/hooks/useListenSell.tsx
@@ -0,0 +1,35 @@
+import { useRef, useState } from "react"
+import { EventSellData } from "../model"
+import { Log } from "viem"
+import { useWatchContractEvent } from "wagmi"
+import { abi } from "@/shared/api"
+
+const useListenSell = () => {
+  const [data, setData] = useState<EventSellData | null>(null)
+  const prevDataRef = useRef<Log[]>([])
+
+  useWatchContractEvent({
+    address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as `0x${string}`,
+    abi: abi,
+    eventName: "TokenListedForSale",
+    onLogs(logs) {
+      if (prevDataRef.current.length > 0 && prevDataRef.current[0].blockHash === logs[0].blockHash) {
+        return;
+      }
+
+      const returnData: EventSellData = {
+        price: logs[0].args.price,
+        seller: logs[0].args.seller,
+        tokenId: logs[0].args.tokenId
+      }
+
+      setData(returnData)
+      prevDataRef.current = logs;
+
+      return { data }
+    }
+
+  })
+}
+
+export { useListenSell }
---
+++ src/features/sell-nft/model/types.ts
@@ -0,0 +1,7 @@
+type EventSellData = {
+  tokenId: bigint | undefined;
+  price: bigint | undefined;
+  seller: string | undefined;
+}
+
+export type { EventSellData }
