143d803db9cde1df574a2949f0dfad869c07cc96
Ororura <hitmanlol890@gmail.com>
2025-03-21 09:05:47 +0300

    refactor(component): update button labels to Russian and simplify conditionals

--- src/widgets/account/ui/component.tsx
+++ src/widgets/account/ui/component.tsx
@@ -15,7 +15,7 @@ const Account: FC = () => {
     <div>
       {ensAvatar && <Image alt='ENS Avatar' src={ensAvatar} />}
       <Button className={'bg-[#3c7a89] hover:bg-[#90d3e3] hover:text-black'} onClick={() => disconnect()}>
-        Disconnect
+        Отключить
       </Button>
     </div>
   );
