ed3afa09e1d2d5fcc20f69be1868e0f9a55308cf
Ororura <hitmanlol890@gmail.com>
2025-04-29 17:56:55 +0300

    feat: enhance NFT sales page with conditional rendering for empty state

--- app/(main)/market/page.tsx
+++ app/(main)/market/page.tsx
@@ -3,12 +3,23 @@
 import { NFTs } from 'widgets/nfts/ui';
 import { FC } from 'react';
 import { getSalesNFT } from 'entities/nft/api';
+import { useStore } from '@tanstack/react-store';
+import { nftStore } from 'entities/nft/models';
 
 const NFTPSalesPage: FC = () => {
+  const items = useStore(nftStore, (state) => state.salesItems);
   return (
     <main className='mx-auto max-w-7xl px-8 py-8 sm:px-4 sm:py-4 md:px-6 md:py-6'>
-      <h1>NFT в продаже</h1>
-      <NFTs fetchData={getSalesNFT} state={(state) => state.salesItems} />
+      {items && items.length !== 0 ? (
+        <>
+          <h1>NFT в продаже</h1>
+          <NFTs fetchData={getSalesNFT} state={(state) => state.salesItems} />
+        </>
+      ) : (
+        <h1 className='animate-fade-in mt-12 rounded-2xl border border-dashed border-gray-300 bg-white/70 px-6 py-12 text-center text-2xl font-semibold text-gray-600 shadow-md backdrop-blur-sm transition-opacity'>
+          😢 В данный момент нет NFT в продаже!
+        </h1>
+      )}
     </main>
   );
 };
