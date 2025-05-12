6b2078d880fe0519950ac881ddba8ae26f63041d
Ororura <hitmanlol890@gmail.com>
2025-05-12 20:01:41 +0300

    feat: implement Sales component for NFT sales page

--- app/(main)/market/page.tsx
+++ app/(main)/market/page.tsx
@@ -1,27 +1,10 @@
-'use client';
-
-import { NFTs } from 'widgets/nfts/ui';
 import { FC } from 'react';
-import { useStore } from '@tanstack/react-store';
-import { nftStore } from 'entities/nft/models';
-import { useGetAllNFTs } from 'entities/nft/hooks/hooks';
+import { Sales } from 'widgets/sales/ui';
 
 const NFTPSalesPage: FC = () => {
-  useGetAllNFTs();
-  const items = useStore(nftStore, (state) => state.items.filter((nft) => nft.forSale));
-
   return (
-    <main className='mx-auto max-w-7xl px-8 py-8 sm:px-4 sm:py-4 md:px-6 md:py-6'>
-      {items && items.length !== 0 ? (
-        <>
-          <h1>NFT в продаже</h1>
-          <NFTs items={items} />
-        </>
-      ) : (
-        <h1 className='animate-fade-in mt-12 rounded-2xl border border-dashed border-gray-300 bg-white/70 px-6 py-12 text-center text-2xl font-semibold text-gray-600 shadow-md backdrop-blur-sm transition-opacity'>
-          😢 В данный момент нет NFT в продаже!
-        </h1>
-      )}
+    <main>
+      <Sales />
     </main>
   );
 };
---
+++ src/widgets/sales/ui/component.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useStore } from '@tanstack/react-store';
+import { useGetAllNFTs } from 'entities/nft/hooks/hooks';
+import { nftStore } from 'entities/nft/models';
+import { FC } from 'react';
+import { NFTs } from 'widgets/nfts/ui';
+
+const Sales: FC = () => {
+  useGetAllNFTs();
+  const items = useStore(nftStore, (state) => state.items.filter((nft) => nft.forSale));
+
+  return (
+    <div className='mx-auto max-w-7xl px-8 py-8 sm:px-4 sm:py-4 md:px-6 md:py-6'>
+      {items && items.length !== 0 ? (
+        <>
+          <h1>NFT в продаже</h1>
+          <NFTs items={items} />
+        </>
+      ) : (
+        <h1 className='animate-fade-in mt-12 rounded-2xl border border-dashed border-gray-300 bg-white/70 px-6 py-12 text-center text-2xl font-semibold text-gray-600 shadow-md backdrop-blur-sm transition-opacity'>
+          😢 В данный момент нет NFT в продаже!
+        </h1>
+      )}
+    </div>
+  );
+};
+
+export { Sales };
